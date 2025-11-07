adc9b3f899adb5397246be70f394bcc69262543d
JosueAranday <josue.aranday01@utrgv.edu>
2025-11-06 21:30:14 -0600

    Update files

--- minpriorityqueue.h
+++ minpriorityqueue.h
@@ -23,7 +23,8 @@ class MinPriorityQueue
 		// Creates an empty MinPriorityQueue
 		MinPriorityQueue()
 		{
-			// TODO
+			H.clear();
+			I.clear();
 		}
 
 		// Returns the number of elements in the MinPriorityQueue.
@@ -31,8 +32,8 @@ class MinPriorityQueue
 		// Must run in O(1) time.
 		int size()
 		{
-			// TODO	
-		}	
+			return H.size();
+		}
 
 		// Pushes a new element x with priority p
 		// into the MinPriorityQueue.
@@ -40,7 +41,12 @@ class MinPriorityQueue
 		// Must run in O(log(n)) time. 
 		void push(T x, int p)
 		{
-			// TODO
+			pair<T, int> couple;						// Create a pair to hold the value and priority
+			couple.first = x;
+			couple.second = p;
+			H.push_back(couple);						// Add the pair to the end of the heap vector
+			int updIndex = bubbleUp(H.size() - 1);		// Bubble up the last index
+			I[x] = updIndex;							// Update the index map
 		}
 
 		// Returns the string at the front of the MinPriorityQueue.
@@ -49,7 +55,7 @@ class MinPriorityQueue
 		// Must run in O(1) time.
 		T front()
 		{
-			// TODO	
+			return (H[0].first);		// The min is always the "root", index 0
 		}
 
 		// Removes the string at the front of the MinPriorityQueue.
@@ -58,7 +64,32 @@ class MinPriorityQueue
 		// Must run in O(log(n)) time. 
 		void pop()
 		{
-			// TODO	
+			if (H.size() == 0) {
+				//cout << "There are no elements to pop." << endl;
+				return;
+			}
+
+			T eraseLater = H[0].first;
+
+			if (H.size() == 1) {
+				H.pop_back();
+				I.erase(eraseLater);
+				return;
+			}
+
+			int sizeH = H.size();
+
+			pair<T, int> couple = H[sizeH - 1];
+			T tempT = H[sizeH - 1].first;
+			H.pop_back();
+
+			H[0] = couple;
+
+			I.erase(eraseLater);
+
+			int updIndex = bubbleDown(0);
+
+			I[tempT] = updIndex;
 		}
 
 		// If s is in the MinPriorityQueue 
@@ -69,7 +100,33 @@ class MinPriorityQueue
 		// Must run in O(log(n)) time. 
 		void decrease_key(T x, int new_p)
 		{
-			// TODO
+			int sizeH = H.size();
+			if (new_p < 0)
+			{
+				cout << "Invalid key decrease, please input a positive integer." << endl;
+				return;
+			}
+
+			if (I.find(x) == I.end())
+				return;
+
+			int updIndex = 0;
+			int foundInd = I[x];
+			int old_p = H[foundInd].second;
+			if (new_p < old_p)
+			{
+				H[foundInd].second = new_p;
+				updIndex = bubbleUp(foundInd);
+				I[x] = updIndex;
+			}
+			else
+			{
+				//H[foundInd].second = new_p;
+				//updIndex = bubbleDown(foundInd);
+				//I[x] = updIndex;
+				cout << "This code doesn't increase the priority." << endl;
+				return;
+			}
 		}
 
 	private:
@@ -77,6 +134,66 @@ class MinPriorityQueue
 		// but you can add some if you want to.
 		unordered_map<T, int> I; // Maps elements to indices in H.
 		vector< pair<T, int> > H;     // The array containing the heap.
+
+		int parent(int i)
+		{
+			return (i - 1) / 2;
+		}
+
+		int childLeft(int i)
+		{
+			return 2 * i + 1;
+		}
+
+		int childRight(int i)
+		{
+			return 2 * i + 2;
+		}
+
+		int bubbleUp(int index)
+		{
+			int i = index;
+
+			while (i > 0 && H[i].second < H[parent(i)].second) 	// while the second member is less than its parent
+			{											// which is a parent violation
+				// Before swapping, update the map 'I' for both items being moved.
+				I[H[i].first] = parent(i);		// The child item is moving to the parent's index
+				I[H[parent(i)].first] = i;  	// The parent item is moving to the child's index
+
+				swap(H[i], H[parent(i)]);	// swap the two couples			
+				i = parent(i);
+			}
+			return i;
+		}
+
+		int bubbleDown(int index)
+		{
+			int i = index;
+			int sizeH = H.size();
+
+			while (childLeft(i) < sizeH)		// Check that at least there's a left child
+			{
+				int smallerChildInd = childLeft(i);
+				if (childRight(i) < sizeH && H[childRight(i)].second < H[childLeft(i)].second)
+				{
+					smallerChildInd = childRight(i);
+				}
+				if (H[i].second > H[smallerChildInd].second)
+				{
+					I[H[i].first] = smallerChildInd;
+					I[H[smallerChildInd].first] = i;
+					swap(H[i], H[smallerChildInd]);
+					i = smallerChildInd;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			return i;
+		}
+
 };
 
 #endif 
--- solve.cpp
+++ solve.cpp
@@ -1,1 +1,129 @@
-// Homework
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_set>
+#include <queue>
+#include <unordered_map>
+#include "solve.h"
+#include "vertex.h"
+
+using namespace std;
+
+string solve(string maze) {
+	unordered_map<int, Vertex*> vertexMap;
+	size_t rowLength = maze.find('\n') + 1;
+	int counter = 0;
+	int xCoor = 0;
+	int yCoor = 0;
+	int xWidth = rowLength - 1;
+	int yHeight = 0;
+
+	while (counter < maze.size()) {		// Runs in O(s) time
+		// Create vertices for each open space in the maze
+		char curr = maze[counter];
+		if (curr == ' ') {
+			vertexMap[counter] = new Vertex(yCoor, xCoor);
+			if (xCoor > 0 && maze[counter - 1] == ' ') {		// Left neighbor
+				vertexMap[counter]->neighs.push_back(vertexMap[counter - 1]);
+				vertexMap[counter - 1]->neighs.push_back(vertexMap[counter]);
+
+			}
+			if (yCoor > 0 && maze[counter - rowLength] == ' ') {		// Top neighbor
+				vertexMap[counter]->neighs.push_back(vertexMap[counter - rowLength]);
+				vertexMap[counter - rowLength]->neighs.push_back(vertexMap[counter]);
+			}
+			//solvedMaze += ' ';
+			xCoor++;
+		}
+		else if (curr == '\n') {
+			yCoor++;
+			xCoor = 0;
+			//solvedMaze += curr;
+		}
+		else {
+			xCoor++;
+			//solvedMaze += curr;
+		}
+		counter++;
+	}
+	yHeight = yCoor;
+
+	//cout << "Map is width: " << xWidth << " and Height: " << yHeight << endl;
+
+	Vertex* startVertex = nullptr;
+	Vertex* endVertex = nullptr;
+	unordered_map<Vertex*, Vertex*> breadCrumbs;
+	unordered_set<Vertex*> marked;
+	queue<Vertex*> Q;
+
+	//	Step 0: Find start and end vertices
+	for (const auto& pair : vertexMap) {		// Runs in O(s) time
+		if (pair.second->row == 0 || pair.second->row == yHeight - 1 ||
+			pair.second->col == 0 || pair.second->col == xWidth - 1) {
+			if (startVertex == nullptr) {
+				startVertex = pair.second;
+			}
+			else {
+				endVertex = pair.second;
+			}
+		}
+	}
+
+	// Step 1: BFS to find path from start to all vertices
+	// Step 1a: Instead of calling breadthFirstSearch(s, breadCrumbs);
+	// startVertex is s
+	// breadCrumbs is bc
+	// marked is marked
+	// Q is Q
+
+	marked.insert(startVertex);
+	Q.push(startVertex);
+
+	while (!Q.empty()) {		// Runs in O(V + E) time
+		Vertex* x = Q.front();
+		Q.pop();
+
+		for (Vertex* y : x->neighs) {
+			if (marked.find(y) == marked.end()) {
+				marked.insert(y);
+				breadCrumbs[y] = x;
+				Q.push(y);
+			}
+		}
+	}
+
+	// Just in case, check if endVertex was reached
+	if (breadCrumbs.find(endVertex) == breadCrumbs.end()) {
+		// No path was found!
+		return "No solution"; // Or return the original maze
+	}
+
+	// Step 2: Trace breadCrumbs from end to start to mark path
+	// from endVertex to startVertex
+
+	Vertex* current = endVertex;
+	while (current != startVertex) {		// Runs in O(d) time, d = distance from start to end
+		int index = current->row * rowLength + current->col;	// Nice calculation to find its position in the original index
+		maze[index] = 'o';
+		current = breadCrumbs[current];
+		//clearScreen();
+		//cout << maze << endl;
+		//std::this_thread::sleep_for(std::chrono::seconds(1)); // Wait 1 second
+	}
+
+	// Mark the start vertex as well
+	int startIndex = startVertex->row * rowLength + startVertex->col;
+	maze[startIndex] = 'o';
+	// Optional: Print out the solved maze at each step
+	//clearScreen();
+	//cout << maze << endl;
+	//std::this_thread::sleep_for(std::chrono::seconds(1)); // Wait 1 second
+
+
+	for (auto& pair : vertexMap) {
+		delete pair.second; // Free the memory
+	}
+
+	// Step 3:
+	return maze;
+}
